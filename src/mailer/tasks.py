b3ef72d50c3ec8e9c2076c395af0f1ddb3714640
Daniel Lima <daniel.lima@ifma.edu.br>
2020-06-18 16:16:46 -0300

    Add tasks for retry deferred and purge log.

--- src/mailer/tasks.py
+++ src/mailer/tasks.py
@@ -3,14 +3,27 @@ from celery.utils.log import get_logger
 
 from django.core import management
 
-app = Celery('django-mailer')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# app = Celery('mailer')
+#
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 
 @shared_task
 def send_mail():
     logger = get_logger(send_mail.__name__)
     logger.info("Sending mails")
-    print("Sending mails")
-    # management.call_command("send_mail", verbosity=0)
+    management.call_command("send_mail", verbosity=0)
+
+
+@shared_task
+def retry_deferred():
+    logger = get_logger(send_mail.__name__)
+    logger.info("Retrying sending deferred mails")
+    management.call_command("retry_deferred", verbosity=0)
+
+
+@shared_task
+def purge_mail_log(days):
+    logger = get_logger(send_mail.__name__)
+    logger.info("Retrying sending deferred mails")
+    management.call_command("purge_mail_log", days=days, verbosity=0)
